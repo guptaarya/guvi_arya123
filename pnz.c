ce67ccb0dac58b58fb09ac4944663b13802acb36
guptaarya <40124602+guptaarya@users.noreply.github.com>
2018-06-10 11:11:35 +0530

    Add files via upload

---
+++ pnz.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("enter the value");
+	scanf("%i",&n);
+	for(i=0;i<=n;i++)
+	{
+	
+	if(n>0)
+	
+		printf("no. is positive");
+	
+	else if(n==0)
+	
+	  
+	  printf("zero");
+	
+	
+	else 
+		
+	
+		printf("no. is negative");
+}
+		return 0;
+	
+}
