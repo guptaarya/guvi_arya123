149cff5d342d4a9704e323f120e42d7e8db48c3c
guptaarya <40124602+guptaarya@users.noreply.github.com>
2018-06-10 11:32:56 +0530

    Add files via upload

---
+++ largest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	if(a>b && a>c)
+	{
+		printf("a is greater");
+	}
+	else if(b>c && b>a)
+	{
+		printf("b is greater");
+	}
+	else 
+	{
+		printf("c is greater");
+	}
+	
+}
