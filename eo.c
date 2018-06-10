f4b66e49937cc30723f7f4ed8b591a00303cfe9e
guptaarya <40124602+guptaarya@users.noreply.github.com>
2018-06-10 11:15:30 +0530

    Add files via upload

---
+++ eo.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	printf("enter the value");
+	scanf("%d",n);
+	for(i=0;i<=10000;i++)
+	{
+		if(i%2==0)
+		{
+			printf("even");
+		}
+			else
+			{
+			
+			printf("odd");
+	}
+	}
+}
+
