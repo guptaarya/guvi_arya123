b72bff3d705c3824cbce375fd4f96da598ec01f5
guptaarya <40124602+guptaarya@users.noreply.github.com>
2018-06-10 14:01:46 +0530

    Add files via upload

---
+++ count.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num,count=0;
+	printf("\nenter the no.");
+	scanf("\n %d",&num);
+	count=printf("%d",num);
+	printf("\n %d digit",count);
+	return 0;
+	
+	
+}
