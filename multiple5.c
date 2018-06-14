d9b79513294682379e4ddcf73847ea1cf4cf396a
guptaarya <40124602+guptaarya@users.noreply.github.com>
2018-06-14 10:31:51 +0530

    Add files via upload

---
+++ multiple5.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int sum=0, i,n;
+	printf("enter any number");
+	scanf("%d",&n);
+	for(i=1;i<=n;i=i++)
+	{
+		printf("\n n * %d = %d",i,5*i);
+		
+	}
+	return 0;
+	}
