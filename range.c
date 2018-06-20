e5cc063ee27c11c40de04147709a30ade0c1d910
guptaarya <40124602+guptaarya@users.noreply.github.com>
2018-06-20 17:44:55 +0530

    Add files via upload

---
+++ range.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	printf("enter 10 elements");
+	scanf("%d",&n);
+		if(n>=1 && n<=10)
+		{
+			printf("yes");
+		}
+		else
+		{
+			printf("no");
+		}
+	
+	return 0;
+}
