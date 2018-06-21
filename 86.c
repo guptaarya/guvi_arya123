65526ddf4fcff9cbcd6bf7879d2fa4af9e23926b
guptaarya <40124602+guptaarya@users.noreply.github.com>
2018-06-21 16:58:29 +0530

    Add files via upload

---
+++ 86.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	char s[100];
+	int i,flag=0;
+	printf("enter any string");
+	scanf("%s",&s);
+	for(i=0;s[i]='\0';i++)
+	{
+		if(s[i]==s[i+1])
+		{
+			flag=1;
+			break;
+		}
+	}
+	if(flag==1)
+	{
+		printf("no");
+		
+	}
+	else
+	{
+		printf("yes");
+	}
+	
+	return 0;
+}
