ab2db702a538264048ff8202c72ce7a870e3ee20
guptaarya <40124602+guptaarya@users.noreply.github.com>
2018-06-21 16:51:32 +0530

    Add files via upload

---
+++ 81.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("\nenter the value of a and b");
+	scanf("%d%d",&a,&b);
+if(a>b)
+	{
+		
+		printf("\n%d",a-b);
+		}
+		else
+		{
+			printf("\n%d",b-a);
+		}
+	return 0;
+}
