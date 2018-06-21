8b60e04d9566bdba0e722b294d9fcc9c1d492bc6
guptaarya <40124602+guptaarya@users.noreply.github.com>
2018-06-21 09:11:31 +0530

    Add files via upload

---
+++ 66a.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int i,num,c=0;
+	printf("enter the number");
+	scanf("%d",&n);
+	 for(i=1;i<=num;i++){
+	 if(num%i==0)
+	 {
+	 c++;
+}
+}
+if (c==2)
+{
+	printf("number is prime");
+}
+else 
+{
+	printf("number is not prime");
+	
+}
+return 0;
+}
