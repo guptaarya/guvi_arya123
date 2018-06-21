9c812e79adc032aea1279c3cecebc09f44dab085
guptaarya <40124602+guptaarya@users.noreply.github.com>
2018-06-21 09:47:03 +0530

    Add files via upload

---
+++ 73.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n,l,r;
+	printf("\nenter any digit");
+	scanf("%d",&n);
+	printf("enter the value of n and r");
+	scanf("%d %d",&n,&r);
+	if(n>l && n<r)
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n no");
+	}
+	return 0;
+}
