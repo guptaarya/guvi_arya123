432692f44936eb70f335dbf879936f58191d8e47
guptaarya <40124602+guptaarya@users.noreply.github.com>
2018-06-21 09:16:45 +0530

    Add files via upload

---
+++ 68.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	printf("enter the multiple of 7");
+	scanf("%d",&n);
+	if(n%7==0)
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
