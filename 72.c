540365617729c89de002dbb062018dfe28f34859
guptaarya <40124602+guptaarya@users.noreply.github.com>
2018-06-21 09:46:02 +0530

    Add files via upload

---
+++ 72.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	char s[100];
+	int i;
+	printf("enter the string");
+	scanf("%s",&s);
+	if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]== 'o' || s[i]=='u')
+	{
+		printf("vowel");
+	}
+	else
+	{
+		printf("not vowel");
+		
+	}
+	return 0;
+}
