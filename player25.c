722cfb305d4d5c6fb98e5d4adb6b0a56bc8e1380
guptaarya <40124602+guptaarya@users.noreply.github.com>
2018-06-23 00:17:32 +0530

    Add files via upload

---
+++ player25.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i,j,temp,a[i],n;
+	printf("enter the elements");
+	scanf("%d",&n);
+	printf("enter the array elements");
+	scanf("%d",&a[i]);
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+			temp=a[i];
+			a[i]==a[j];
+			a[j]=temp;
+		}
+		printf("ascending number are");
+		for(i=0;i<n;i++)
+		{
+			scanf("%d",&a);
+		}
+}
+}
+	return 0;
+}
