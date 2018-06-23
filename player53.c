cb11b93e55338f83490fff86d7baf7f24bc721d9
guptaarya <40124602+guptaarya@users.noreply.github.com>
2018-06-23 10:52:20 +0530

    Add files via upload

---
+++ player53.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	char str[100];
+	int i,count=0;
+	printf("enter the string");
+	scanf("%d",str);
+	for(i=0;str='\0';i++)
+	{
+		count=count+1;
+	}
+	printf("%d",count);
+	return 0;
+}
