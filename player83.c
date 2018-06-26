2736eb948edb6e55cf5198028615e847c8d8ddff
guptaarya <40124602+guptaarya@users.noreply.github.com>
2018-06-26 10:19:25 +0530

    Add files via upload

---
+++ player83.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n;
+	scanf("%d",&n);
+	int array[n],i,res=0;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d ",&array[i]);
+	}
+	for(i=0;i<n;i++)#include <stdio.h>
+
+int main(void) 
+{
+	int n,res;
+	scanf("%d",&n);
+	res=~n;
+	printf("%d",res);
+	return 0;
+}
+	{
+		res=(res)|(array[i]);
+	}
+	printf("%d",res);
+	return 0;
+}
